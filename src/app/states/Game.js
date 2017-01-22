04638a714c2edf1a3228cb898c0603ad6b2e28f2
Miguel Gasca <miguelgasca@gmail.com>
2017-01-22 20:55:04 +0100

    Inicial

---
+++ src/app/states/Game.js
@@ -0,0 +1,18 @@
+/*
+ * Game state
+ * ============================================================================
+ *
+ * A sample Game state, displaying the Phaser logo.
+ */
+
+
+export default class Game extends Phaser.State {
+
+  create () {
+    this.game.stage.backgroundColor = '#324e92';
+  }
+
+  update () {
+  }
+
+}
